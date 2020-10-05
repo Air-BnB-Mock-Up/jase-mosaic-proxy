ad035de8d100c6fff6666b3387fe743b7fec581c
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-05 16:11:58 -0700

    Create initial repo with 3 services

--- client/air-photos/jase-mosaic/server/index.js
+++
@@ -1,9 +0,0 @@
-/////////////////////////////////////////
-// START SERVER /////////////////////////
-const app = require('./httpHandler');
-const port = 1000;
-  app.listen(port, () => {
-    console.log(`Served on localhost:${port}`);
-  })
-
-
--- client/reviews/javier-component/client/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-var location = Math.floor((Math.random() * 100) + 1);
-
-ReactDOM.render(<App locationID={location}/>, document.getElementById('app'));
