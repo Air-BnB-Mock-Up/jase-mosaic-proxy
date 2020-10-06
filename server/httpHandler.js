5e1ae7be24a5040238c0603d49b749a1302d7216
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-05 21:35:45 -0700

    Render all 3 services through proxy

--- server/httpHandler.js
+++ server/httpHandler.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const cors = require('../cors.json');
 const express = require('express');
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/../dist')));
+
 
 
 module.exports = app;
