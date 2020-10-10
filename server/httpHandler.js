0f4664e3d5a5f03e049a26c8628bcac5467291bf
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-10 10:20:44 -0700

    :wq

--- server/httpHandler.js
+++ server/httpHandler.js
@@ -6,12 +6,10 @@ const app = express();
 ///////////////////////////////////////////
 // HELPERS ////////////////////////////////
 const cors = require('../cors.json');
-const { template } = require('./template.js')
+const { template } = require('./template.js');
 ///////////////////////////////////////////
 app.get('/airbnb/room/:locationID', (req, res) => {
-  console.log(req.params.locationID)
-  locationID = req.params.locationID;
-    res.header(cors)
+    res.header(cors);
     res.send(template);
-})
+});
 module.exports.app = app;
